b570a01c987441c9ac8a0eb424878da8ab22c7d9
fool19 <71994280+fool19@users.noreply.github.com>
2021-10-25 15:28:06 +0200

    Add files via upload

---
+++ Player.java
@@ -0,0 +1,38 @@
+package simplifiedProjet;
+import java.util.Scanner;
+import java.util.List;
+
+public class Player {
+	public String name;
+	public List<RumourCard> rumourCardListPlayer;
+	public Player(String name, List<RumourCard> rumourCardListP) {
+		this.name = name;
+		this. rumourCardListPlayer= rumourCardListP;
+	}
+	void accuse(Player player) {
+		System.out.println(this.name+" accuse "+player.name);
+	}
+	void Hunt(Player player) {
+		try (Scanner in = new Scanner(System.in)) {
+			System.out.println("Which card do you want to use?");
+			int i = in.nextInt();
+			System.out.print(this.name);
+			rumourCardListPlayer.get(i).SkillHunt();
+		}
+		System.out.println(player.name);
+	}
+	void witch(Player player) {
+		System.out.print(this.name);
+		rumourCardListPlayer.get(1).SkillWitch();
+		System.out.println(player.name);
+		
+	}
+	public void ToString() {
+		System.out.println(name+" and cards with ");
+		for (int i = 0; i < rumourCardListPlayer.size(); i++) {
+			rumourCardListPlayer.get(i).ToString();
+		}
+		System.out.println();
+	}
+
+}
