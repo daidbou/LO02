efe0e6b42101d9d610cb87ff21761519d3af5d59
fool19 <71994280+fool19@users.noreply.github.com>
2021-11-11 00:16:39 +0100

    Add files via upload

---
+++ RumourCard3.java
@@ -0,0 +1,28 @@
+package simplifiedProjet;
+
+public class RumourCard3 implements RumourCard{
+	@Override
+	public Player SkillWitch() {
+		System.out.print(" Skill Witch RumourCard3 ");	
+		return null;
+	}
+
+	@Override
+	public Player SkillHunt() {
+		System.out.print(" Skill Hunt RumourCard3 ");
+		return null;
+	}
+
+	@Override
+	public void ToString() {
+		
+		System.out.print(" RumourCard3 ");
+	}
+
+	@Override
+	public String Name() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
