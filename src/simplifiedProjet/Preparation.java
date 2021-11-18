87f07b73b25312c7c620f3ee4020d166cbecc0d0
daidbou <89360632+daidbou@users.noreply.github.com>
2021-11-18 22:00:54 +0100

    correction of names

--- src/simplifiedProjet/Preparation.java
+++ src/simplifiedProjet/Preparation.java
@@ -3,55 +3,57 @@ import java.util.ArrayList;
 import java.util.List;
 
 import simplifiedProjet.RumourCard.RumourCard;
-import simplifiedProjet.RumourCard.RumourCard1;
-import simplifiedProjet.RumourCard.RumourCard10;
-import simplifiedProjet.RumourCard.RumourCard11;
-import simplifiedProjet.RumourCard.RumourCard12;
-import simplifiedProjet.RumourCard.RumourCard2;
-import simplifiedProjet.RumourCard.RumourCard3;
-import simplifiedProjet.RumourCard.RumourCard4;
-import simplifiedProjet.RumourCard.RumourCard5;
-import simplifiedProjet.RumourCard.RumourCard6;
-import simplifiedProjet.RumourCard.RumourCard7;
-import simplifiedProjet.RumourCard.RumourCard8;
-import simplifiedProjet.RumourCard.RumourCard9;
+import simplifiedProjet.RumourCard.AngryMob;
+import simplifiedProjet.RumourCard.BlackCat;
+import simplifiedProjet.RumourCard.Broomstick;
+import simplifiedProjet.RumourCard.Cauldron;
+import simplifiedProjet.RumourCard.DuckingStool;
+import simplifiedProjet.RumourCard.EvilEye;
+import simplifiedProjet.RumourCard.HookedNose;
+import simplifiedProjet.RumourCard.PetNewt;
+import simplifiedProjet.RumourCard.PointedHat;
+import simplifiedProjet.RumourCard.TheInquisition;
+import simplifiedProjet.RumourCard.Toad;
+import simplifiedProjet.RumourCard.Wart;
 
 public interface Preparation {
 
-    static RumourCard1 rumourCard1 = new RumourCard1();
-	static RumourCard2 rumourCard2 = new RumourCard2();
-	static RumourCard3 rumourCard3 = new RumourCard3();		
-    static RumourCard4 rumourCard4 = new RumourCard4();	
-    static RumourCard5 rumourCard5 = new RumourCard5();
-    static RumourCard6 rumourCard6 = new RumourCard6();
-    static RumourCard7 rumourCard7 = new RumourCard7();
-    static RumourCard8 rumourCard8 = new RumourCard8();
-    static RumourCard9 rumourCard9 = new RumourCard9();
-    static RumourCard10 rumourCard10 = new RumourCard10();
-    static RumourCard11 rumourCard11 = new RumourCard11();
-    static RumourCard12 rumourCard12 = new RumourCard12();//create 12 rumour cards
+    static AngryMob angryMob = new AngryMob();
+    static BlackCat blackCat = new BlackCat();
+    static Broomstick broomstick = new Broomstick();
+    static Cauldron cauldron = new Cauldron();
+    static DuckingStool duckingStool = new DuckingStool();
+    static EvilEye evilEye = new EvilEye();
+    static HookedNose hookedNose = new HookedNose();
+    static PetNewt petNewt = new PetNewt();
+    static PointedHat pointedHat = new PointedHat();
+    static TheInquisition theInquisition = new TheInquisition();
+    static Toad toad = new Toad();
+    static Wart wart = new Wart();//create 12 rumour cards
 
 
     public static List<RumourCard> rumourCardList = new ArrayList<RumourCard>(){{
 		//add(null);
-        add(rumourCard1);
-		add(rumourCard2);
-		add(rumourCard3);
-		add(rumourCard4);
-        add(rumourCard5);
-        add(rumourCard6);
-        add(rumourCard7);
-        add(rumourCard8);
-        add(rumourCard9);
-        add(rumourCard10);
-        add(rumourCard11);
-        add(rumourCard12);
+        add(angryMob);
+		add(blackCat);
+		add(broomstick);
+		add(cauldron);
+        add(duckingStool);
+        add(evilEye);
+        add(hookedNose);
+        add(petNewt);
+        add(pointedHat);
+        add(theInquisition);
+        add(toad);
+        add(wart);
 
 	}};//add 12 rumour cards to a list
 			
 	
 	//static List<RumourCard> rumourCardListP1 = rumourCardList.subList(0, 2);//it's players own rumourcard list
 	//static List<RumourCard> rumourCardListP2 = rumourCardList.subList(2, 4);// needs to be controlled, it's not random yet
+
+    // /!\ 18/11/2021 don't forget to name the card with the correct name
 
     public static List<RumourCard> rumourCardListP1 = new ArrayList<RumourCard>(){{
         //add(null);
