8392c56ff04ab35fb6fb69fa471a02c3efe07dc7
daidbou <89360632+daidbou@users.noreply.github.com>
2021-11-23 17:36:48 +0100

    Update Broomstick.java

--- src/simplifiedProjet/RumourCard/Broomstick.java
+++ src/simplifiedProjet/RumourCard/Broomstick.java
@@ -1,6 +1,8 @@
 package simplifiedProjet.RumourCard;
 
 import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
 
 import simplifiedProjet.Engine;
 import simplifiedProjet.Player;
@@ -36,7 +38,6 @@ public class Broomstick implements RumourCard{
 		System.out.println("Take next turn");
 		return Engine.nextPlayer(playerList, Engine.nameToPlayer(playerList, namePTurn2));
 	}
-
 	@Override
 	public Player skillWitchBot(String namePTurn2, List<Player> playerList) {
 		System.out.println("Take next turn");
@@ -44,16 +45,33 @@ public class Broomstick implements RumourCard{
 	}
 
 	@Override
-	public Player skillHunt(String namePturn1, List<Player> playerList) {
-		System.out.println("Take next turn");
-		return Engine.nextPlayer(playerList, Engine.nameToPlayer(playerList, namePturn1));
+	public Player skillHunt(String hunter, List<Player> playerList) {
+		System.out.println("please select one player (p1 or b1 for example :");
+		for(Player p: playerList){
+			System.out.println(p.getName());
+		}
+		
+		Scanner sc = new Scanner(System.in);
+		String hunted = sc.nextLine();
+		
+		for(Player p:playerList){
+			if(p.getName().equals(hunted) && !p.ifIsOutOfTurn()){
+				return Engine.nameToPlayer(playerList, hunted);
+			}
+		}
+		sc.close();
+
+		return null; 
 	}
 
 	@Override
-	public Player skillHuntBot(String namePturn2, List<Player> playerList) {
-		System.out.println("Take next turn");
-		return Engine.nextPlayer(playerList, Engine.nameToPlayer(playerList, namePturn2));
+	public Player skillHuntBot(String hunter, List<Player> playerList) {
+		Random rand = new Random();
+		Player hunted = new Player();
+		while(true){
+			hunted = playerList.get(rand.nextInt(playerList.size()-1));
+			if(!hunted.ifIsOutOfTurn()){
+				return hunted;
+			}}
+		}
 	}
- 
-    
-}
